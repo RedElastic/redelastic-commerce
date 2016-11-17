0a3ad0267977e1336a291f120d11c6af7bb00f58
jasongoodwin <jasongoodwin@google.com>
2016-11-17 09:34:36 -0500

    fix test in GithubAccountService

--- test/contexts/GithubAccountServiceIntegrationTest.java
+++ test/contexts/GithubAccountServiceIntegrationTest.java
@@ -1,9 +1,16 @@
 package contexts;
 
+import akka.actor.ActorSystem;
+import contexts.account.api.Account;
+import contexts.account.live.GithubAccountService;
 import org.junit.Before;
 import org.junit.Test;
 import play.libs.ws.WSClient;
 import play.libs.ws.WS;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -14,27 +21,27 @@ import static org.junit.Assert.assertEquals;
  */
 public class GithubAccountServiceIntegrationTest {
 
-    //GithubAccountService service;
+    GithubAccountService service;
 
     @Before
     public void setup() {
         WSClient ws = WS.newClient(play.api.test.Helpers.testServerPort());
-        //service = new GithubAccountService(ws);
+        service = new GithubAccountService(ws, ActorSystem.create("testSystem"));
     }
 
     @Test
     public void shouldReturnAccountFromGithub() throws Exception {
-        //Invoke the service to get the account async
-        //CompletionStage<Account> cs = service.getAccount("jasongoodwin");
+        //Invoke the service to get the account asynchronously
+        CompletionStage<Account> cs = service.getAccount("jasongoodwin");
 
         //We need to have the thread wait for the result in the test or it will succeed regardless of result
         //We cast the abstract CompletionStage  to the CompletableFuture impl to have access to the get method
         //Note: It's only appropriate to block w/ get() in test contexts!
-        //Account account = ((CompletableFuture<Account>)cs).get(1, TimeUnit.SECONDS);
+        Account account = ((CompletableFuture<Account>)cs).get(1, TimeUnit.SECONDS);
 
         //Check the result from github
-        //assertEquals("jasongoodwin", account.getUserId());
-        //assertEquals("Jason Goodwin", account.getName());
-        //assertEquals("https://avatars.githubusercontent.com/u/2845228?v=3", account.getImageUri());
+        assertEquals("jasongoodwin", account.getUserId());
+        assertEquals("Jason Goodwin", account.getName());
+        assertEquals("https://avatars.githubusercontent.com/u/2845228?v=3", account.getImageUri());
     }
 }
