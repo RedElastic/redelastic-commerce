4cb080d558c2b582400a50e6f650867dd178aaf0
Kevin Webber <kevin.webber@redelastic.com>
2017-04-22 17:58:23 -0400

    working cart with UI

--- app/controllers/CartController.java
+++ app/controllers/CartController.java
@@ -1,9 +1,13 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.inject.Inject;
 import contexts.cart.api.CartItem;
 import contexts.cart.api.CartService;
+import contexts.order.api.OrderedItem;
+import contexts.product.api.Product;
+import contexts.product.api.ProductService;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -15,14 +19,30 @@ import java.util.concurrent.CompletionStage;
 
 public class CartController extends Controller {
     final private CartService service;
+    final private ProductService productService;
 
     @Inject
-    public CartController(CartService service) {
+    public CartController(CartService service, ProductService productService) {
         this.service = service;
+        this.productService = productService;
     }
 
     public CompletionStage<Result> getCart(String userId) {
-        return service.getCartContents(userId).thenApply(cart -> ok(Json.toJson(cart)));
+        return service.getCartContents(userId).thenApply(cart -> {
+            List<DisplayCartItem> items = new ArrayList<>();
+
+            for (CartItem item : cart) {
+                Product product = productService.getProduct(item.getProductId());
+                items.add(new DisplayCartItem(
+                    item.getProductId(),
+                    product.getName(),
+                    product.getDescription(),
+                    item.getQuantity(),
+                    item.getPrice()));
+            }
+
+            return ok(Json.toJson(items));
+        });
     }
 
     public Result updateCart() {
@@ -43,4 +63,33 @@ public class CartController extends Controller {
         service.emptyCart(userId);
         return ok();
     }
+}
+
+class DisplayCartItem {
+    private UUID productId;
+    private String name;
+    private String description;
+    private Integer quantity;
+    private Double price;
+
+    public DisplayCartItem(UUID productId, String name, String description, Integer quantity, Double price) {
+        this.productId = productId;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public DisplayCartItem() {}
+
+    public UUID getProductId() { return productId; }
+
+    public String getName() { return name; }
+
+    public Integer getQuantity() { return quantity; }
+
+    public Double getPrice() { return price; }
+
+    public String getDescription() { return description; }
+
 }
